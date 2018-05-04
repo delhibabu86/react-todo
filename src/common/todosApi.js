647bcad59b00d14f997e067ef205a361da51d6d9
DVengamBhanumoorthy <DVengamBhanumoorthy@spherasolutions.com>
2018-05-04 01:21:56 -0400

    Trying out travis CI

--- src/common/todosApi.js
+++ src/common/todosApi.js
@@ -2,7 +2,7 @@
  * Created by DVengamBhanumoorthy on 2/22/2018.
  */
 
-const apiUrl = 'https://5a8b8dea3d92490012370c03.mockapi.io/api/v1/todos';
+const apiUrl = 'http://5a8b8dea3d92490012370c03.mockapi.io/api/v1/todos';
 
 export default () =>  fetch(apiUrl, {
   cache: 'no-cache',
